6148779565aedb99c87fefe5ca71b84419333708
Tiffany Yu <102394354+ukii-1119@users.noreply.github.com>
2023-10-18 00:29:06 -0700

    publishing lab3

---
+++ labs/lab03/lab.py
@@ -0,0 +1,95 @@
+# lab.py
+
+
+import os
+import io
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+# ---------------------------------------------------------------------
+# QUESTION 1
+# ---------------------------------------------------------------------
+
+
+def clean_universities(df):
+    ...
+
+def university_info(cleaned):
+    ...
+
+
+
+# ---------------------------------------------------------------------
+# QUESTION 2
+# ---------------------------------------------------------------------
+
+
+def std_scores_by_nation(cleaned):
+    ...
+    
+
+def su_and_spread():
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 3
+# ---------------------------------------------------------------------
+
+
+def read_linkedin_survey(dirname):
+    ...
+
+
+def com_stats(df):
+    ...
+
+
+
+# ---------------------------------------------------------------------
+# QUESTION 4
+# ---------------------------------------------------------------------
+
+
+def read_student_surveys(dirname):
+    ...
+
+
+def check_credit(df):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 5
+# ---------------------------------------------------------------------
+
+
+def most_popular_procedure(pets, procedure_history):
+    ...
+
+def pet_name_by_owner(owners, pets):
+    ...
+
+
+def total_cost_per_city(owners, pets, procedure_history, procedure_detail):
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 6
+# ---------------------------------------------------------------------
+
+
+def average_seller(sales):
+    ...
+
+def product_name(sales):
+    ...
+
+def count_product(sales):
+    ...
+
+def total_by_month(sales):
+    ...
